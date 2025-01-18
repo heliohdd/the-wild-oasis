06e718c462743c40668a2f32707cfd1c1d357fa0
Helio Dourado <heliohdd@yahoo.com.br>
2025-01-18 19:03:44 -0300

    Section 28.369 - Detecting a Click Outside the Modal

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,22 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect, useRef } from "react";
+
+export function useOutsideClick(handler, listenCapturing = true) {
+  const ref = useRef();
+
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) {
+          handler();
+        }
+      }
+
+      document.addEventListener("click", handleClick, listenCapturing);
+
+      return removeEventListener("click", handleClick, listenCapturing);
+    },
+    [handler, listenCapturing]
+  );
+  return ref;
+}
